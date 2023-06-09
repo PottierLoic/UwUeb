3b765313532af7de21241b53a2df680bf57841f6
Loïc Pottier <loic.pottier@live.fr>
2023-06-10 01:11:12 +0200

    button transition

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
-var circle_index = document.getElementById("circle_index");
-var circle_cv = document.getElementById("circle_cv");
-var circle_portfolio = document.getElementById("circle_portfolio");
-var circle_blog = document.getElementById("circle_blog");
-var circle_connect = document.getElementById("circle_connect");
-var circle_contact = document.getElementById("circle_contact");
+var circle_index = document.getElementById("circle-index");
+var circle_cv = document.getElementById("circle-cv");
+var circle_portfolio = document.getElementById("circle-portfolio");
+var circle_blog = document.getElementById("circle-blog");
+var circle_connect = document.getElementById("circle-connect");
+var circle_contact = document.getElementById("circle-contact");
+var circle_start = document.getElementById("starting-circle");
 
 var button_index = document.getElementById("index-button");
 var button_cv = document.getElementById("cv-button");
@@ -12,107 +13,108 @@ var button_blog = document.getElementById("blog-button");
 var button_connect = document.getElementById("connect-button");
 var button_contact = document.getElementById("contact-button");
 
-document.addEventListener("DOMContentLoaded", start);
-
 button_index.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêche le comportement par défaut du lien
-  var href = button_index.querySelector("a").href; // Récupère la valeur de l'attribut href du lien à l'intérieur du bouton
+  event.preventDefault();
+  var href = button_index.querySelector("a").href;
 
-  circle_index.style.width = "300vw";
-  circle_index.style.height = "300vh";
+  circle_index.style.width = "10000px";
+  circle_index.style.height = "10000px";
   circle_index.style.top = "-" + window.innerHeight + "px";
   circle_index.style.opacity = "1";
 
   setTimeout(function() {
     window.location.href = href;
-  }, 500);
+  }, 750);
 });
 
 button_cv.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêche le comportement par défaut du lien
-  var href = button_cv.querySelector("a").href; // Récupère la valeur de l'attribut href du lien à l'intérieur du bouton
+  event.preventDefault();
+  var href = button_cv.querySelector("a").href;
 
-  circle_cv.style.width = "300vw";
-  circle_cv.style.height = "300vh";
+  circle_cv.style.width = "10000px";
+  circle_cv.style.height = "10000px";
   circle_cv.style.top = "-" + window.innerHeight + "px";
   circle_cv.style.opacity = "1";
 
   setTimeout(function() {
     window.location.href = href;
-  }, 500);
+  }, 750);
 });
 
 button_portfolio.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêche le comportement par défaut du lien
-  var href = button_portfolio.querySelector("a").href; // Récupère la valeur de l'attribut href du lien à l'intérieur du bouton
+  event.preventDefault();
+  var href = button_portfolio.querySelector("a").href;
 
-  circle_portfolio.style.width = "300vw";
-  circle_portfolio.style.height = "300vh";
+  circle_portfolio.style.width = "10000px";
+  circle_portfolio.style.height = "10000px";
   circle_portfolio.style.top = "-" + window.innerHeight + "px";
   circle_portfolio.style.opacity = "1";
 
   setTimeout(function() {
     window.location.href = href;
-  }, 500);
+  }, 750);
 });
 
 button_blog.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêche le comportement par défaut du lien
-  var href = button_blog.querySelector("a").href; // Récupère la valeur de l'attribut href du lien à l'intérieur du bouton
+  event.preventDefault();
+  var href = button_blog.querySelector("a").href;
 
-  circle_blog.style.width = "300vw";
-  circle_blog.style.height = "300vh";
+  circle_blog.style.width = "10000px";
+  circle_blog.style.height = "10000px";
   circle_blog.style.top = "-" + window.innerHeight + "px";
   circle_blog.style.opacity = "1";
 
   setTimeout(function() {
     window.location.href = href;
-  }, 500);
+  }, 750);
 });
 
 button_connect.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêche le comportement par défaut du lien
-  var href = button_connect.querySelector("a").href; // Récupère la valeur de l'attribut href du lien à l'intérieur du bouton
+  event.preventDefault();
+  var href = button_connect.querySelector("a").href;
 
-  circle_connect.style.width = "300vw";
-  circle_connect.style.height = "300vh";
+  circle_connect.style.width = "10000px";
+  circle_connect.style.height = "10000px";
   circle_connect.style.top = "-" + window.innerHeight + "px";
   circle_connect.style.opacity = "1";
 
   setTimeout(function() {
     window.location.href = href;
-  }, 500);
+  }, 750);
 });
 
 button_contact.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêche le comportement par défaut du lien
-  var href = button_contact.querySelector("a").href; // Récupère la valeur de l'attribut href du lien à l'intérieur du bouton
+  event.preventDefault();
+  var href = button_contact.querySelector("a").href;
 
-  circle_contact.style.width = "300vw";
-  circle_contact.style.height = "300vh";
+  circle_contact.style.width = "10000px";
+  circle_contact.style.height = "10000px";
+  circle_contact.style.top = "-" + window.innerHeight + "px";
   circle_contact.style.opacity = "1";
 
   setTimeout(function() {
     window.location.href = href;
-  }, 500);
+  }, 750);
 });
 
 function set_circle_position(circle, element) {
-  var rect = button.getBoundingClientRect();
-  var buttonCenterX = rect.left + rect.width / 2;
-  var buttonBottomY = rect.bottom;
+  var rect = element.getBoundingClientRect();
+  var center_y = rect.top;
 
-  circle.style.top = buttonBottomY+50 + "px";
-  circle.style.left = buttonCenterX - circle.offsetWidth / 2 + "px";
+  circle.style.top = center_y + "px";
 }
 
-function start() {
+window.onload = function() {
   set_circle_position(circle_index, button_index);
   set_circle_position(circle_cv, button_cv);
   set_circle_position(circle_portfolio, button_portfolio);
   set_circle_position(circle_blog, button_blog);
   set_circle_position(circle_connect, button_connect);
   set_circle_position(circle_contact, button_contact);
-}
+
+  set_circle_position(circle_start, circle_start.parentNode);
+  circle_start.style.width = '0';
+  circle_start.style.height = '0';
+};
 
 
