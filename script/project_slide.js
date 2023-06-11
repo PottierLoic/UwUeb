5b5c26fa04cb31097c7636f3751d50f92d364ac2
Loïc Pottier <loic.pottier@live.fr>
2023-06-11 14:26:29 +0200

    cursor + slide anim finished

--- script/project_slide.js
+++ script/project_slide.js
@@ -1,8 +1,5 @@
 const image_left = document.querySelectorAll('.project-image-left');
 const image_right = document.querySelectorAll('.project-image-right');
-const text_left = document.querySelectorAll('.project-text-left');
-const text_right = document.querySelectorAll('.project-text-right');
-
 
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
@@ -20,14 +17,6 @@ function animateElements() {
     image_right.forEach((element) => {
         if (isInViewport(element)) 
             element.classList.add('project-image-right--visible');
-    });
-    text_left.forEach((element) => {
-        if (isInViewport(element)) 
-            element.classList.add('project-text-left--visible');
-    });
-    text_right.forEach((element) => {
-        if (isInViewport(element)) 
-            element.classList.add('project-text-right--visible');
     });
 }
 
